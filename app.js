3814883ba9bde849a0987317fb9c7ba748377a32
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2022-01-14 18:10:08 +0100

    build:instalar y configurar express-validator'

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+
+    // gestionando error de validación
+    if (err.array) {
+      // error de validación
+      err.status = 422;
+      const errInfo = err.array({ onlyFirstError: true })[0];
+      err.message = `(${errInfo.location}) ${errInfo.param} ${errInfo.msg}`;
+    }
+    
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
+const { query, validationResult } = require('express-validator');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/adslist', [
+  // validaciones
+  query('talla').isNumeric().withMessage('debe ser numérica'),
+  query('color').custom(color => { return color === 'red';}).withMessage('solo vale red') // validación custom
+], (req, res, next) => {
+  validationResult(req).throw();
+  const talla = req.query.talla;
+  const color = req.query.color;
+
+  console.log(req.query);
+
+  res.send(`ok la talla ${talla} y del color ${color}`);
+});
+
+router.get('/tagslist', [
+  // validaciones
+  query('talla').isNumeric().withMessage('debe ser numérica'),
+  query('color').custom(color => { return color === 'red';}).withMessage('solo vale red') // validación custom
+], (req, res, next) => {
+  validationResult(req).throw();
+  const talla = req.query.talla;
+  const color = req.query.color;
+
+  console.log(req.query);
+
+  res.send(`ok la talla ${talla} y del color ${color}`);
+});
+
+router.post('/newad', (req, res, next) => {
+  const nombre = req.body.nombre;
+
+  console.log(req.body);
+
+  res.send(`Recibido el nombre ${nombre}`);
 });
 
 module.exports = router;
